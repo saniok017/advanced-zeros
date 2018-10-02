40755f0ddc9a50ca8df9d19a1768b75b2fdd04c1
saniok017 <saniok017@gmail.com>
2018-10-03 00:15:21 +0300

    advanced-zeros/8thTask: almost completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  const factorArray = [];
+  let zeroesQuantity = 0;
+  // finding prime factors for base
+  for (let tempBase =base, i=2; i <= tempBase; i++){
+    let power = 0
+    while (tempBase % i === 0){
+      factorArray.push([i, power += 1]);
+      tempBase /= i;
+    }
+  }
+  // picking biggest prime factor
+  const BIGGESTFACTOR =factorArray.slice(-1).pop();
+  let factor =BIGGESTFACTOR[0];
+  let pow = BIGGESTFACTOR[1];
+
+  // counting zeroes 
+    let tempNumber = number;
+    let tempFactor = factor;
+    while (tempNumber/tempFactor >= 1){
+      zeroesQuantity += Math.floor(tempNumber/tempFactor);
+      tempFactor *= factor;
+    }
+  return Math.floor(zeroesQuantity/pow);
 }
